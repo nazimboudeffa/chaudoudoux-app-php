ebc0e18b7cd9b44751e2db35681a6f92a889140a
nazimboudeffa <boudeffa@gmail.com>
2021-10-17 16:28:22 +0200

    Typo error

--- js/register.js
+++ js/register.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
               if ($("#word").length > 0){
                 return AddWord();
               } else {
-                window.location.href = "profil.php";
+                window.location.href = "profile.php";
               }
             }
             if(data.error === true){
